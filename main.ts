e081a0b21840c0ddd7c35ab0af0aa6c5f7cb4c13
saulbailen21-cell <saulbailen21@institutramonbarbat.cat>
2025-11-12 14:01:44 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,23 @@
+let obstacle = 0
 basic.showIcon(IconNames.Heart)
-let obstacle = cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters)
-if (obstacle < 15) {
-    cuteBot.stopcar()
-} else {
-    cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0x0000ff)
-    cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0xff0080)
-    cuteBot.forward()
-    basic.pause(200)
-    cuteBot.motors(100, 400)
-    basic.pause(1000)
-    cuteBot.forward()
-    basic.pause(200)
-    cuteBot.forward()
-    basic.pause(200)
-    cuteBot.motors(40, 100)
-    basic.pause(1000)
-    cuteBot.forward()
-    basic.pause(200)
-}
 basic.forever(function () {
-	
+    obstacle = cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters)
+    if (obstacle < 15) {
+        cuteBot.stopcar()
+    } else {
+        cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0x0000ff)
+        cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0xff0080)
+        cuteBot.forward()
+        basic.pause(200)
+        cuteBot.motors(100, 400)
+        basic.pause(1000)
+        cuteBot.forward()
+        basic.pause(200)
+        cuteBot.forward()
+        basic.pause(200)
+        cuteBot.motors(40, 100)
+        basic.pause(1000)
+        cuteBot.forward()
+        basic.pause(200)
+    }
 })
