053b7e5e651e43c95908b37338cddc48743b5716
saulbailen21-cell <saulbailen21@institutramonbarbat.cat>
2025-11-12 13:53:00 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,21 @@
-cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0x0000ff)
-cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0xff0080)
-cuteBot.forward()
-basic.pause(200)
-cuteBot.motors(100, 400)
-basic.pause(1000)
-cuteBot.forward()
-basic.pause(200)
-cuteBot.forward()
-basic.pause(200)
-cuteBot.motors(40, 100)
-basic.pause(1000)
-cuteBot.forward()
-basic.pause(200)
 basic.forever(function () {
-	
+    let obstacle = 0
+    if (obstacle < 15) {
+        cuteBot.stopcar()
+    } else {
+        cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0x0000ff)
+        cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0xff0080)
+        cuteBot.forward()
+        basic.pause(200)
+        cuteBot.motors(100, 400)
+        basic.pause(1000)
+        cuteBot.forward()
+        basic.pause(200)
+        cuteBot.forward()
+        basic.pause(200)
+        cuteBot.motors(40, 100)
+        basic.pause(1000)
+        cuteBot.forward()
+        basic.pause(200)
+    }
 })
